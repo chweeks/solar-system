2335ad51d4ed03e957918dd7d484ebfd114cc5c5
chweeks <chrisharryweeks@gmail.com>
2015-10-07 16:48:17 +0100

    added sim year button

--- application.js
+++ application.js
@@ -3,7 +3,7 @@ $( document ).ready (function() {
   var earth = new Planet(5.97e24, 6371e3, 149.6e9)
   var sun = new Sun
 
-  $( "#addDay" ).click(function( event ) {
+  $( "#simDay" ).click(function( event ) {
 
     earth.addDay(sun);
 
@@ -12,6 +12,21 @@ $( document ).ready (function() {
        left: earth.xPixelCoord()+'px',
        top: '0px',
        right: '0px',
-     });
+     }, 50);
    });
+
+   $( "#simYear" ).click(function( event ) {
+
+     for(i=0; i < 365; i++){
+
+       earth.addDay(sun);
+
+       $("#earth").animate({
+          bottom:  earth.yPixelCoord()+'px',
+          left: earth.xPixelCoord()+'px',
+          top: '0px',
+          right: '0px',
+        }, 1);
+      };
+    });
 });
